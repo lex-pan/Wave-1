708445a335dbb5e31ee44b7deeba54eb9bec40eb
lex <lexiang.pan@gmail.com>
2020-04-08 18:50:13 -0400

    deleting unrelated files

--- field_area.py
+++
@@ -1,7 +0,0 @@
-length = int(input("Length of farmer's field:"))
-width = int(input("Width of farmer's field"))
-
-numOfAcres = (length*width)/43560
-numOfAcres = str(numOfAcres)
-
-print("The area of the field in acres is" + numOfAcres)
