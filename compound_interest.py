8cb246364f8909ab83cc4e7c0c125664482bc693
lex <lexiang.pan@gmail.com>
2020-04-08 18:38:48 -0400

    first commit

---
+++ compound_interest.py
@@ -0,0 +1,8 @@
+initialAmount = int(input())
+
+year1 = initialAmount + initialAmount*0.04
+print(round(year1, 2))
+year2 = year1 + year1*0.04
+print(round(year2, 2))
+year3 = year2 + year2*0.04
+print(round(year3, 2))
