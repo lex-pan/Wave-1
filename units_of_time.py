cf5cea338345d850fdb59dd10527a62036137c36
lex <lexiang.pan@gmail.com>
2020-04-17 21:03:49 -0400

    units of time

---
+++ units_of_time.py
@@ -0,0 +1,6 @@
+number_of_days = int(input("Number Of days:"))
+number_of_hours = int(input("Number of hours:"))
+number_of_minutes = int(input("Number of minutes:"))
+number_of_seconds = int(input("Number of seconds:"))
+
+print(number_of_days * 86400 + number_of_hours * 3600 + number_of_minutes * 60 + number_of_seconds)
